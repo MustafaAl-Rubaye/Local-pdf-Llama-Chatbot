925e257cfabe7f3fab6ba4751955c5f08f262473
Mustafa Al-Rubaye <48747643+MustafaAl-Rubaye@users.noreply.github.com>
2025-07-22 18:38:12 +0300

    Add files via upload

---
+++ main_ollama.py
@@ -0,0 +1,44 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from input_pdfs import retriever
+
+
+model = OllamaLLM(model="llama3")
+
+temp = """
+Answer the question below.
+
+Here is the pdf information to use: {context}
+
+Question: {question}
+
+Answer: 
+"""
+
+
+prompt = ChatPromptTemplate.from_template(temp)
+
+
+chain = prompt | model
+
+def make_bot():
+
+    print("Welcome to DocBot!     Type 'exit', 'bye' to stop.")
+    while True:
+        my_input = input("You: ")
+        if my_input.lower() in ["exit", "bye"]:
+            break
+
+        #results = model.invoke(input="hello world")
+        #context = retriever.invoke(my_input)
+        docs = retriever.invoke(my_input)
+        context = "\n\n".join(doc.page_content for doc in docs)
+        results = chain.invoke({"context": context, "question":my_input})
+        print("DocBot: ", results)
+        # building a chat history
+        context += f"\nUser: {my_input}\nAI: {results}"
+
+
+if __name__ == "__main__":
+    make_bot()
+
